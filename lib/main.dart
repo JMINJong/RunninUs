c00fd2785ae0af70311cf7afd693f100be91880b
iamjooon2 <iamjooon2@gmail.com>
2022-04-27 23:15:15 +0900

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:runnin_us/screen/google_map.dart';
+import 'package:runnin_us/screen/run/google_map.dart';
 import 'package:runnin_us/screen/home_screen.dart';
 import 'package:runnin_us/screen/init_screen.dart';
-import 'package:runnin_us/screen/login.dart';
+import 'package:runnin_us/screen/auth/login.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: InitScreen(),
     theme: ThemeData(
-primarySwatch: Colors.grey,
+      primarySwatch: Colors.grey,
     ),
   ));
 }
