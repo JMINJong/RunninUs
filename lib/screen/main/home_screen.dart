6151b65955e636b04d3ad5e6474042123fc91e3a
iamjooon2 <iamjooon2@gmail.com>
2022-05-19 16:20:57 +0900

    예약실 빠졌던거 다시 넣기

--- lib/screen/main/home_screen.dart
+++ lib/screen/main/home_screen.dart
@@ -57,7 +57,7 @@ class _HomeScreenState extends State<HomeScreen> {
               },
               children: [
                 MainScreen(),
-                WaitingRoomScreen(),
+                ReservedRoomScreen(),
                 MyPageScreen(),
                 StoreScreen(),
               ],
