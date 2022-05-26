ecfa9e6faf7a09fe78f437407951e3d240d66ca3
iamjooon2 <iamjooon2@gmail.com>
2022-05-26 23:42:12 +0900

    유저 정보 업데이트 API

---
+++ lib/api/update_user_info.dart
@@ -0,0 +1,26 @@
+import 'package:runnin_us/const/dummy.dart';
+import 'package:dio/dio.dart';
+import 'api_generator.dart';
+
+Future<bool> updateUserInfoApi() async {
+
+  try {
+    var dio = await Dio().request(
+      getApi(API.UPDATE_USER_INFO),
+      data: {
+        "user_id": myPageList[0]['uid'],
+        "user_nick": myPageList[0]['name'],
+        "user_age": myPageList[0]['age'],
+        "user_sex": myPageList[0]['sex'],
+        "user_address": myPageList[0]['location'],
+      },
+      options: Options(method: 'POST'),
+    );
+    var updatedUserInfo=dio.data['message'];
+    print(updatedUserInfo);
+    return true;
+  } catch (e) {
+    print(e);
+    return false;
+  }
+}
