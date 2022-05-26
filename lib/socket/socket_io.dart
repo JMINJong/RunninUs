19ff3cf8398e14c8c880da5f31b608eaf3e0e726
JMINJong <wjsalswhd55@gachon.ac.kr>
2022-05-26 22:23:11 +0900

    socket io

---
+++ lib/socket/socket_io.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:socket_io_client/socket_io_client.dart';
+
+// STEP1:  Stream setup
+class StreamSocket {
+  final _socketResponse = StreamController();
+
+  void Function(dynamic) get addResponse => _socketResponse.sink.add;
+
+  Stream get getResponse => _socketResponse.stream;
+
+  void dispose() {
+    _socketResponse.close();
+  }
+}
+//ws://echo.websocket.org
+//http://runninus-api.befined.com:8000
+
+//STEP2: Add this function in main function in main.dart file and add incoming data to the stream
+void connectAndListen() {
+  IO.Socket socket = IO.io('http://runninus-api.befined.com:8000',
+      OptionBuilder().setTransports(['websocket']).build());
+
+  socket.onConnect((_) {
+    print('connect');
+    // socket.emit('msg', 'test');
+    // socket.emit('PING');
+  });
+  // socket.emit('PING');
+
+  //When an event recieved from server, data is added to the stream
+  socket.on('event', (data) => StreamSocket().addResponse);
+  socket.on('PONG', (data) {
+
+    // print(data);
+    print('PONG');
+    StreamSocket().addResponse('pong');
+  });
+  socket.onDisconnect((_) => print('disconnect'));
+}
+
+void socketTest() {
+  IO.Socket socket = IO.io('http://runninus-api.befined.com:8000');
+
+  socket.emit('PING');
+}
